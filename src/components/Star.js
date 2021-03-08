5880069bcc361fc602af591b42260f30c6836b1e
cordenbrock <cordenbrock@gmail.com>
2021-03-08 12:13:47 -0800

    Solve styled components issue not rendering correctly by adding in unit value

--- src/components/Star.js
+++ src/components/Star.js
@@ -3,24 +3,14 @@ import styled from 'styled-components';
 
 function Star(props) {
 
-  // const starStyle = {
-  //   position: "absolute",
-  //   borderRadius: 13,
-  //   boxShadow: '0px 0px 16px #9E9E9E',
-  //   zIndex: 2,
-  //   cursor: 'pointer',
-  //   bottom: props.position.bottom,
-  //   right: props.position.right
-  // }
-
   const Button = styled.button`
   position: absolute;
-  border-radius: 13;
+  border-radius: 13px;
   box-shadow: 0px 0px 16px #9E9E9E;
   z-index: 2;
   cursor: pointer;
-  bottom: ${props => props.position.bottom ? props.position.bottom : props.position.bottom};
-  right: ${props => props.position.right ? props.position.right : props.position.right};
+  bottom: ${props.position.bottom}px;
+  right: ${props.position.right}px;
   `;
 
   return (
