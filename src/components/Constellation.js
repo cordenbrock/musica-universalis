5c2a7e1c04a01d8f37a261b19c4779cad36f07c0
cordenbrock <cordenbrock@gmail.com>
2021-03-10 09:40:07 -0800

    Fix typo on coord var, loop through last ten constellation objects, draw up to 10 star constellation

--- src/components/Constellation.js
+++ src/components/Constellation.js
@@ -1,26 +1,34 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 function Constellation(props) {
   const { constellation } = props;
-  console.log(constellation)
   const canvasRef = useRef(null);
 
 
   const draw = (ctx) => {
+    ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
     ctx.strokeStyle = 'white'
     ctx.shadowBlur = 10;
     ctx.shadowColor = "white";
-    ctx.lineWidth = .314;
+    ctx.lineWidth = .618;
     
-    if (constellation.length > 1) {
+    // if (constellation.length > 1) {
       ctx.beginPath();
-      ctx.moveTo(constellation[constellation.length-2].xCoordinate, constellation[constellation.length-2].xCoordinate);
-      ctx.lineTo(constellation[constellation.length-1].xCoordinate, constellation[constellation.length-1].yCoordinate);
-      ctx.stroke();
+      let i = 0;
+      if (constellation.length > 10){
+        i = constellation.length-10;
       }
+      for (i; i < constellation.length-1; i++) {
+        ctx.moveTo(constellation[i].xCoordinate, constellation[i].yCoordinate);
+        ctx.lineTo(constellation[i+1].xCoordinate, constellation[i+1].yCoordinate);  
+      }
+      
+      ctx.stroke();
+      // }
     }
 
   useEffect(() => {
+    console.log(constellation);
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
 
