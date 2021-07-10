8e8a6ba7edd2e8c6f8b88a26d48b14fca440c546
Cory Nordenbrock <cordenbrock@gmail.com>
2021-07-10 14:52:40 -0700

    Integrate google analytics for prod env only

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import GA4React from "ga-4-react";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const ga4react = new GA4React(process.env.REACT_APP_GOOGLE_ANALYTICS);
+const isProduction = process.env.NODE_ENV === 'production';
+
+(async _ => {
+  isProduction && (await ga4react.initialize());
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+})();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
